fd256e180c5e3912ecbfd4664b2f4ce29cb9ad95
rohitgurjar <rohitgurjar0085@gmail.com>
2025-01-02 11:48:05 +0530

    - bug fix

--- src/app/banner/index.js
+++ src/app/banner/index.js
@@ -8,7 +8,7 @@ function Banner() {
         {/* Left Content */}
         <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
           <h2 className="text-3xl font-bold text-purple-800 mb-2">
-            HI I&apos;AM
+            HI,
           </h2>
           <h1 className="text-5xl font-extrabold text-blue-700 mb-2">
             Rohit Gurjar
